919d2b3f625ca1d342dc60160d746e3173f12834
Shishir-0 <prasadshishir1@gmail.com>
2026-08-29 16:30:29 +0530

    chore: remove untracked dev/out build artifacts

--- dev/out/dev/static/chunks/dev_219uq1s._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/node_modules_next_dist_20wefz_._.js","static/chunks/_1-clgec._.js","static/chunks/node_modules_three_build_three_core_18d0oq6.js","static/chunks/node_modules_three_build_three_module_03rsyo6.js","static/chunks/node_modules_1c_8_0_._.js","static/chunks/src_styles_0irm2rj.css"],
-    source: "entry"
-});
